94a66b117bd49ef85fa399c8b25d26fd33960621
datapsycho <mr.data.psycho@gmail.com>
2023-02-26 03:58:22 +0100

    Adding tracing before sink CH: 120

--- scripts/curl_test.sh
+++ scripts/curl_test.sh
@@ -2,3 +2,7 @@ echo "Hitting Main End point"
 RESPONSE=$(curl 'http://127.0.0.1:8000/health_check')
 echo "response: $RESPONSE"
 echo "==="
+
+curl -X POST  http://127.0.0.1:8000/subscriptions \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "name=le%20guin&email=ursula_le_guin%40gmail.com"
